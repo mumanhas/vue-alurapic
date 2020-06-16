bfc06227f69bd951e298b36fd10b6f308fcda76d
Murilo <murilomanhas@gmail.com>
2020-06-15 23:51:38 -0300

    class 04 done

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,67 @@
 <template>
-  <div>
-    <h1>{{ title }}</h1>
-    <ul>
-      <li v-for="photo of photos" :key="photo.id">
-        <img :src="photo.url" :alt="photo.titulo">
+  <div class="corpo">
+
+    <h1 class="titulo">{{ titulo }}</h1>
+
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotos" :key="foto.id">
+
+        <painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+        </painel>
+
       </li>
     </ul>
+
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import Painel from './components/shared/painel/Painel';
 
 export default {
-  name: 'App',
+
+  components: {
+    painel : Painel 
+  },
 
   data() {
     return {
-      title : "Alurapic",
-      photos: [] 
+      titulo : "Alurapic",
+      fotos: [] 
     }
   },
 
   created() {
     axios.get('http://localhost:3000/v1/fotos')
-    .then(res => this.photos = res.data, err => console.log(err))
+    .then(res => this.fotos = res.data, err => console.log(err))
   }
 }
 </script>
+
+<style>
+
+  .titulo {
+    text-align: center;
+  }
+
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+  .imagem-responsiva {
+    width: 100%;
+  }
+
+</style>
